87d98958937d7da8a60eb176d3984b96944e50bf
VirtualVulpes <59158087+VirtualVulpes@users.noreply.github.com>
2021-09-30 19:51:43 -0500

    Added Snake Game

---
+++ grid.js
@@ -0,0 +1,16 @@
+const GRID_WIDTH = 37
+const GRID_HEIGHT = 26
+
+export function randomGridPosition() {
+    return {
+        x: Math.floor(Math.random() * GRID_WIDTH) + 1,
+        y: Math.floor(Math.random() * GRID_HEIGHT) + 1
+    }
+}
+
+export function outsideGrid(position) {
+    return (
+        position.x < 1 || position.x > GRID_WIDTH ||
+        position.y < 1 || position.y > GRID_HEIGHT
+    )
+}
